3a8f2fe6f78e554b135d0d7d4660c708e085c584
tommy-carlos williams <tommy@devgeeks.org>
2017-02-24 11:13:36 +1100

    Added tests for App.vue to chase 100% coverage

---
+++ template/test/unit/specs/App.spec.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'; // eslint-disable-line no-unused-vars
+// Import F7
+import Framework7 from 'framework7'; // eslint-disable-line no-unused-vars
+
+// Import F7 Vue Plugin
+import Framework7Vue from 'framework7-vue';
+
+import App from 'src/App';
+
+let vm;
+
+// Init F7 Vue Plugin
+Vue.use(Framework7Vue);
+
+describe('App.vue', () => {
+  beforeEach(() => {
+    vm = new Vue({ // eslint-disable-line no-new
+      el: document.createElement('div'),
+      render: h => h(App),
+      // Init Framework7 by passing parameters here
+      // The absolute minimum is an empty routes array
+      framework7: {
+        routes: [],
+      },
+    });
+  });
+
+  describe('Computed', () => {
+    it('should return a computed property for the global `isiOS`', () => {
+      expect(App.isiOS).to.equal(window.isiOS);
+    });
+  });
+});
