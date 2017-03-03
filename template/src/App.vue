7d4f10d4f336e025aa620822b48204c9046cbd38
tommy-carlos williams <tommy@devgeeks.org>
2017-03-03 11:38:57 +1100

    Add default route

--- template/src/App.vue
+++ template/src/App.vue
@@ -7,27 +7,19 @@
 
     <!-- Main Views -->
     <f7-views>
-      <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
+      <f7-view iid="main-view" url="/" :init="true" :dynamic-navbar="true" navbar-through main>
         <!-- Navbar -->
         <f7-navbar v-if="isiOS" title="Home"></f7-navbar>
         <!-- Pages -->
-        <f7-pages>
-          <!-- pages from the routes will go here...replacing `home-page` -->
-          <home-page />
-        </f7-pages>
+        <f7-pages />
       </f7-view>
     </f7-views>
   </div>
 </template>
 
 <script>
-  import Home from './components/pages/Home';
-
   export default {
     name: 'App',
-    components: {
-      'home-page': Home,
-    },
     computed: {
       isiOS() {
         return window.isiOS;
--- template/src/routes.js
+++ template/src/routes.js
@@ -2,6 +2,10 @@ import Home from './components/pages/Home';
 
 export default [
   {
+    path: '/',
+    component: Home,
+  },
+  {
     path: '/home/',
     component: Home,
   },
