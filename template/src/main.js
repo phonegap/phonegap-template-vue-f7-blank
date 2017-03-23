a48557bf83551c88e55b60541f455b3929e15b1f
tommy-carlos williams <tommy@devgeeks.org>
2017-03-23 13:28:26 +1100

    Added conditional Android backbutton behaviour
    Fixes #5

--- template/src/main.js
+++ template/src/main.js
@@ -61,10 +61,28 @@ new Vue({ // eslint-disable-line no-new
   }
 });
 
+function handleBackButton () {
+  // NOTE: The back button will behave differently depending on circumstance
+  // If the side panel is open, close it
+  if (document.querySelector('.panel-left.active')) {
+    // This will do nothing when the split-view is open
+    return window.f7.closePanel();
+  }
+  // Close modals
+  // @TODO How to handle modals we shouldn't close like a login-screen?
+  if (document.querySelector('.modal-in')) {
+    return f7.closeModal();
+  }
+  // If we have a back button, we want it to go back
+  if (document.querySelector('a.back')) {
+    return f7.mainView.router.back();
+  }
+  // Default to closing the app
+  return window.navigator.app.exitApp();
+}
+
 // Ye olde Device Ready
 document.addEventListener('deviceready', () => {
   // Bind to the back button for Android
-  document.addEventListener('backbutton', () => {
-    f7.mainView.router.back();
-  });
+  document.addEventListener('backbutton', handleBackButton);
 });
