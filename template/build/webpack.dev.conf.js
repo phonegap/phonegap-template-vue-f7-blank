644a04bf7d380469aa795d8db8040378ca9d3a84
Ondrej Valenta <me@ondrejvalenta.com>
2017-09-13 02:31:37 +0200

    Updated to Webpack2 and the latest F7 and VueJS
    As well as babel, eslint and so on

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -1,38 +1,40 @@
 /* eslint-disable import/no-extraneous-dependencies, no-var, vars-on-top */
 
-var config = require('../config');
-var webpack = require('webpack');
-var merge = require('webpack-merge');
-var utils = require('./utils');
-var baseWebpackConfig = require('./webpack.base.conf');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var FriendlyErrors = require('friendly-errors-webpack-plugin');
+// Webpack2 config
+
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach((name) => {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name]);
-});
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
 
 module.exports = merge(baseWebpackConfig, {
   module: {
-    loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap }),
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
-  // eval-source-map is faster for development
-  devtool: '#eval-source-map',
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': config.dev.env,
+      'process.env': config.dev.env
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'src/index.html',
-      inject: true,
+      template: 'index.html',
+      inject: true
     }),
-    new FriendlyErrors(),
-  ],
-});
+    new FriendlyErrorsPlugin()
+  ]
+})
+
