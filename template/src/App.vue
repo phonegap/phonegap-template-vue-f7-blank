44198def320ec036f6704dcdf84dfcb0414b79ae
tommy-carlos williams <tommy@devgeeks.org>
2017-11-14 11:37:15 +1100

    Moved back button handler to App.vue

--- template/src/App.vue
+++ template/src/App.vue
@@ -20,10 +20,34 @@
 <script>
   export default {
     name: 'App',
+    methods: {
+      handleBackButton () {
+        // NOTE: The back button will behave differently depending on circumstance
+        // If the side panel is open, close it
+        if (document.querySelector('.panel-left.active')) {
+          // This will do nothing when the split-view is open
+          return this.$f7.closePanel();
+        }
+        // Close modals
+        // @TODO How to handle modals we shouldn't close like a login-screen?
+        if (document.querySelector('.modal-in')) {
+          return this.$f7.closeModal();
+        }
+        // If we have a back button, we want it to go back
+        if (this.$f7.mainView.history.length > 1) {
+          return this.$f7.mainView.router.back();
+        }
+        // Default to closing the app
+        return window.navigator.app.exitApp();
+      }
+    },
     computed: {
       isiOS () {
         return window.isiOS;
       }
+    },
+    created () {
+      document.addEventListener('backbutton', this.handleBackButton);
     }
   };
 </script>
--- template/src/main.js
+++ template/src/main.js
@@ -59,29 +59,3 @@ new Vue({ // eslint-disable-line no-new
     app: App
   }
 });
-
-function handleBackButton () {
-  // NOTE: The back button will behave differently depending on circumstance
-  // If the side panel is open, close it
-  if (document.querySelector('.panel-left.active')) {
-    // This will do nothing when the split-view is open
-    return f7.closePanel();
-  }
-  // Close modals
-  // @TODO How to handle modals we shouldn't close like a login-screen?
-  if (document.querySelector('.modal-in')) {
-    return f7.closeModal();
-  }
-  // If we have a back button, we want it to go back
-  if (f7.mainView.history.length > 1) {
-    return f7.mainView.router.back();
-  }
-  // Default to closing the app
-  return window.navigator.app.exitApp();
-}
-
-// Ye olde Device Ready
-document.addEventListener('deviceready', () => {
-  // Bind to the back button for Android
-  document.addEventListener('backbutton', handleBackButton);
-});
