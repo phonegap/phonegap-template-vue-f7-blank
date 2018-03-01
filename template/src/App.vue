86d043cc3fb378a30d712c1483487ac562dcfe33
Simon MacDonald <simon.macdonald@gmail.com>
2018-02-28 22:19:09 -0500

    🐛 fix closing of dialog and side panel

--- template/src/App.vue
+++ template/src/App.vue
@@ -19,14 +19,14 @@ export default {
     handleBackButton() {
       // NOTE: The back button will behave differently depending on circumstance
       // If the side panel is open, close it
-      if (document.querySelector('.panel-left.active')) {
+      if (document.querySelector('.panel-left.panel-active')) {
         // This will do nothing when the split-view is open
-        return this.$f7.closePanel();
+        return this.$f7.panel.close();
       }
       // Close modals
       // @TODO How to handle modals we shouldn't close like a login-screen?
       if (document.querySelector('.modal-in')) {
-        return this.$f7.closeModal();
+        return this.$f7.popover.close();
       }
       // If we have a back button, we want it to go back
       if (this.$f7.views.main.router.history.length > 1) {
