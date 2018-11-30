eada1eee0be476ad2210e3f22be3203c302b4c3e
timkim <timk@adobe.com>
2018-11-30 15:27:05 -0800

    Fix paths for tests

--- template/test/unit/specs/Home.spec.js
+++ template/test/unit/specs/Home.spec.js
@@ -3,13 +3,13 @@
 import Vue from 'vue';
 
 // Import F7
-import Framework7 from 'framework7/dist/framework7.esm.bundle.js';
+import Framework7 from 'framework7/framework7.esm.bundle.js';
 
 // Import F7 Vue Plugin
-import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js';
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js';
 
 // Import F7 Styles
-import Framework7Styles from 'framework7/dist/css/framework7.css';
+import Framework7Styles from 'framework7/css/framework7.css';
 
 import Home from '@/pages/home';
 
