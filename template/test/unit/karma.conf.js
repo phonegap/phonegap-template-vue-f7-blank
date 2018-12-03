ac97bd9afac6b84e1b7e60ad0fee651f4738024d
timkim <timk@adobe.com>
2018-12-03 14:44:33 -0800

    Upgrading karma, removing phantom, and using karma chrome instead

--- template/test/unit/karma.conf.js
+++ template/test/unit/karma.conf.js
@@ -11,8 +11,14 @@ module.exports = function karmaConfig (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    browsers: ['Chrome_without_security'],
+    customLaunchers: {
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security']
+      }
+    },
+    frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
