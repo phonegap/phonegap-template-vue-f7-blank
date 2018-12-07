fba1b72993544d7df89e5324057856b263f8c801
timkim <timk@adobe.com>
2018-12-07 11:48:29 -0800

    Remove tests and update to latest webpack

--- template/config/test.env.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, no-var, vars-on-top */
-
-var merge = require('webpack-merge');
-var devEnv = require('./dev.env');
-
-module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-});
